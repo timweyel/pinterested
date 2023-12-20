cfff3581c6cf2ffd4eeb8ff121f39e80fbf3f732
Tim Weyel <tweyel@gmail.com>
2023-12-19 16:23:07 -0800

    add logout functionality and conditional nav buttons

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home/about'
+  
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
